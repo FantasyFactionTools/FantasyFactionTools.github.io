656e0bc006a31136a7afab06188a87190a1dec63
Christopher Chapman <christopher.chapman@disney.com>
2017-04-21 11:26:01 -0700

    updates

--- scripts/lgbt-characters.js
+++ scripts/lgbt-characters.js
@@ -2,15 +2,13 @@
 authors["Alternative history"] = [];
 authors["Alternative history"].push({"author":"Mary Gentle", "series":"Book of Ash", "book-title":"Ash: A Secret History"});
 authors["Alternative history"].push({"author":"Anne Lyle", "series":"Night's Masque", "book-title":"The Alchemist of Souls"});
-authors["Alternative history"].push({"author":"SM Stirling", "series":"Island in the Sea of Time", "book-title":""});
+authors["Alternative history"].push({"author":"SM Stirling", "series":"Island in the Sea of Time", "book-title":"Island in the Sea of Time"});
 
 authors["Epic Fantasy"] = [];
 authors["Epic Fantasy"].push({"author":"Peter V Brett", "series":"The Demon Cycle", "book-title":"The Painted Man"});
 authors["Epic Fantasy"].push({"author":"Jacquelyn Carey", "series":"Kushiel's Universe", "book-title":"Kushiel's Dart"});
 authors["Epic Fantasy"].push({"author":"Jacquelyn Carey", "series":"Namaah's Trilogy", "book-title":"Namaah's Kiss"});
 authors["Epic Fantasy"].push({"author":"Seth Dickinson", "series":"", "book-title":"The Traitor Baru Cormorant"});
-authors["Epic Fantasy"].push({"author":"David Gemmell", "series":"The Drenai Saga", "book-title":"Quest for Lost Heroes"});
-authors["Epic Fantasy"].push({"author":"John Gwynne", "series":"The Faithful and the Fallen ", "book-title":"Malice"});
 authors["Epic Fantasy"].push({"author":"Robin Hobb", "series":"Rain Wild Chronicles", "book-title":"The Dragon Keeper"});
 authors["Epic Fantasy"].push({"author":"Kameron Hurley", "series":"Worldbreaker Saga", "book-title":"The Mirror Empire"});
 authors["Epic Fantasy"].push({"author":"Alex Marshall", "series":"The Crimson Empire", "book-title":"A Crown for Cold Silver"});
@@ -20,7 +18,6 @@ authors["Epic Fantasy"].push({"author":"Mark Charan Newton", "series":"Legends o
 authors["Epic Fantasy"].push({"author":"Den Patrick", "series":"Erebus Sequence", "book-title":"The Boy With the Porcelain Blade"});
 authors["Epic Fantasy"].push({"author":"Ricardo Pinto", "series":"The Stone Dance of the Chameleon", "book-title":"The Chosen"});
 authors["Epic Fantasy"].push({"author":"W.G. Saraband", "series":"", "book-title":"The Shattered Sands"});
-authors["Epic Fantasy"].push({"author":"Ken Scholes", "series":"Psalms of Isaak", "book-title":"Lamentation"});
 authors["Epic Fantasy"].push({"author":"Anna Stephens", "series":"", "book-title":"Godblind"});
 authors["Epic Fantasy"].push({"author":"Brent Weeks", "series":"Nigh Angel", "book-title":"The Way of the Shadows"});
 authors["Epic Fantasy"].push({"author":"Django Wexler", "series":"The Shadow Campaigns", "book-title":"The Thousand Names"});
@@ -30,13 +27,18 @@ authors["Fairy Tales"] = [];
 authors["Fairy Tales"].push({"author":"Jim C.Hines", "series":"Princess", "book-title":"Red Hood's Revenge"});
 authors["Fairy Tales"].push({"author":"Malinda Lo", "series":"", "book-title":"Ash"});
 
+authors["Heroic Fantasy"] = [];
+authors["Heroic Fantasy"].push({"author":"David Gemmell", "series":"The Drenai Saga", "book-title":"Quest for Lost Heroes"});
+authors["Heroic Fantasy"].push({"author":"Jen Williams", "series":"The Winnowing Flame Trilogy", "book-title":"The Ninth Rain"});
+
 authors["High Fantasy"] = [];
 authors["High Fantasy"].push({"author":"Joe Abercrombie", "series":"First Law World", "book-title":"Sharp Ends"});
 authors["High Fantasy"].push({"author":"Peter V Brett", "series":"The Demon Cycle", "book-title":"The Painted Man"});
 authors["High Fantasy"].push({"author":"Amanda Downum", "series":"The Necromancer Chronicles", "book-title":"The Bone Palace"});
 authors["High Fantasy"].push({"author":"Lynn Flewelling", "series":"Nightrunner", "book-title":"Luck in the Shadows"});
-authors["High Fantasy"].push({"author":"John Gwynne", "series":"The Faithful and the Fallen", "book-title":"Malice"});
 authors["High Fantasy"].push({"author":"Kameron Hurley", "series":"Worldbreaker Saga", "book-title":"The Mirror Empire"});
+authors["High Fantasy"].push({"author":"Mark Lawrence", "series":"Book of the Ancestor", "book-title":"Red Sister"});
+authors["High Fantasy"].push({"author":"Rebecca Levene", "series":"The Hollow Gods", "book-title":"Smiler's Fair"});
 authors["High Fantasy"].push({"author":"Alex Marshall", "series":"The Crimson Empire", "book-title":"A Crown for Cold Silver"});
 authors["High Fantasy"].push({"author":"Mark Charan Newton", "series":"Legends of the Red Sun", "book-title":"Nights of Villjamur"});
 authors["High Fantasy"].push({"author":"Den Patrick", "series":"Erebus Sequence ", "book-title":"The Boy With the Porcelain Blade"});
@@ -51,12 +53,15 @@ authors["Historic Fantasy"].push({"author":"Ellen Klages", "series":"", "book-ti
 authors["Horror"] = [];
 authors["Horror"].push({"author":"Caitlin R. Kiernan", "series":"", "book-title":"The Drowning Girl"});
 
+authors["Low Fantasy"] = [];
+authors["Low Fantasy"].push({"author":"N.K. Jemsin", "series":"The Broken Earth", "book-title":"The Fifth Season"});
+
 authors["Military Fantasy"] = [];
 authors["Military Fantasy"].push({"author":"Megan Derr", "series":"Kria", "book-title":"Prisoner"});
 authors["Military Fantasy"].push({"author":"Django Wexler", "series":"The Shadow Campaigns", "book-title":"The Thousand Names"});
 
 authors["Mythology"] = [];
-authors["Mythology"].push({"author":"Rebecca Levene", "series":"The Hollow Gods", "book-title":"Smiler's Fair"});
+authors["Mythology"].push({"author":"Sarah Monette", "series":"Iskryne World", "book-title":"A companion to wolves"});
 authors["Mythology"].push({"author":"Rick Riordan", "series":"The Heroes of Olympus", "book-title":"The Lost Hero"});
 
 authors["New Weird"] = [];
@@ -72,20 +77,19 @@ authors["Romance"].push({"author":"Ellen Kushner", "series":"The World of Rivers
 authors["Romance"].push({"author":"Mercedes Lackey", "series":"Valdemar:The Last Herald Mage", "book-title":"Magic's Pawn"});
 authors["Romance"].push({"author":"Malinda Lo", "series":"", "book-title":"Ash"});
 authors["Romance"].push({"author":"Sarah Monette", "series":"Doictrine of Labyrinths", "book-title":"Melusine"});
-authors["Romance"].push({"author":"Sarah Monette", "series":"Iskryne World", "book-title":"A Companion to Wolves"});
 authors["Romance"].push({"author":"Ricardo Pinto", "series":"The Stone Dance of the Chameleon", "book-title":"The Chosen"});
 authors["Romance"].push({"author":"B.R. Sanders", "series":"Ariah", "book-title":"Ariah"});
 authors["Romance"].push({"author":"Kai Ashante Wilson", "series":"The Sorcerer of the Wildeeps", "book-title":"The Sorcerer of the Wildeeps"});
-authors["Romance"].push({"author":"Elizabeth Bear", "series":"Jacob's Ladder", "book-title":"Dust"});
 
-authors["Sci-fi"] = [];
-authors["Sci-fi"].push({"author":"Becky Chambers", "series":"Wayfares", "book-title":"The Long Way to a Small, Angry Planet"});
-authors["Sci-fi"].push({"author":"Storm Constantine", "series":"Wraeththu", "book-title":"The Enchantments of Flesh and Spirit"});
-authors["Sci-fi"].push({"author":"Seth Dickinson", "series":"", "book-title":"The Traitor Baru Cormorant"});
-authors["Sci-fi"].push({"author":"N.K. Jemisin", "series":"The Broken Earth", "book-title":"The Fifth Season"});
-authors["Sci-fi"].push({"author":"Kameron Hurley", "series":"Worldbreaker Saga", "book-title":"Mirror Empire"});
-authors["Sci-fi"].push({"author":"David Mitchell", "series":"", "book-title":"Cloud Atlas"});
-authors["Sci-fi"].push({"author":"Elizabeth Bear", "series":"Karen Memory", "book-title":"Karen Memory"});
+authors["Sci-Fi"] = [];
+authors["Sci-Fi"].push({"author":"Elizabeth Bear", "series":"Jacob's Ladder", "book-title":"Dust"});
+authors["Sci-Fi"].push({"author":"Becky Chambers", "series":"Wayfares", "book-title":"The Long Way to a Small, Angry Planet"});
+authors["Sci-Fi"].push({"author":"Storm Constantine", "series":"Wraeththu", "book-title":"The Enchantments of Flesh and Spirit"});
+authors["Sci-Fi"].push({"author":"Eli Freysson", "series":"The Golden Throne", "book-title":"The War of the Usurper"});
+authors["Sci-Fi"].push({"author":"Seth Dickinson", "series":"", "book-title":"The Traitor Baru Cormorant"});
+authors["Sci-Fi"].push({"author":"Kameron Hurley", "series":"Worldbreaker Saga", "book-title":"Mirror Empire"});
+authors["Sci-Fi"].push({"author":"David Mitchell", "series":"", "book-title":"Cloud Atlas"});
+authors["Sci-Fi"].push({"author":"Elizabeth Bear", "series":"Karen Memory", "book-title":"Karen Memory"});
 
 authors["Steampunk"] = [];
 authors["Steampunk"].push({"author":"Gail Carriger", "series":"Parasol Protectorate", "book-title":"Soulless"});
@@ -101,14 +105,14 @@ authors["Urban Fantasy"].push({"author":"Ilona Andrews", "series":"Kate Daniels"
 authors["Urban Fantasy"].push({"author":"Max Gladstone", "series":"Craft Sequence", "book-title":"Three Parts Dead"});
 authors["Urban Fantasy"].push({"author":"J.A. Pitts", "series":"Sarah Beauhall", "book-title":"Black Blades Blue"});
 authors["Urban Fantasy"].push({"author":"Tom Pollock", "series":"Skyscraper Trilogy", "book-title":"The City's Son"});
-authors["Urban Fantasy"].push({"author":"V.E. Schwab", "series":"Shades of Magic", "book-title":"A Gathering of Shadows"});
+authors["Urban Fantasy"].push({"author":"V.E. Schwab", "series":"Shades of Magic", "book-title":"A Conjuring of Light"});
 authors["Urban Fantasy"].push({"author":"Maggie Stiefvater", "series":"The Raven Cycle", "book-title":"The Raven Boys"});
-authors["Urban Fantasy"].push({"author":"Elizabeth Bear", "series":"Karen Memory", "book-title":"Karen Memory"});
 
 authors["Western"] = [];
-authors["Western"].push({"author":"L. Frank Baum", "series":"Oz", "book-title":"The Marvelous Land of Oz"});
+authors["Western"].push({"author":"Elizabeth Bear", "series":"Karen Memory", "book-title":"Karen Memory"});
 
 authors["Young Adult"] = [];
+authors["Young Adult"].push({"author":"L. Frank Baum", "series":"Oz", "book-title":"The Marvelous Land of Oz"});
 authors["Young Adult"].push({"author":"Leigh Bardugo", "series":"The Grisha", "book-title":"Shadow and Bone"});
 authors["Young Adult"].push({"author":"Malinda Lo", "series":"", "book-title":"Ash"});
 authors["Young Adult"].push({"author":"Rick Riordan", "series":"The Heroes of Olympus", "book-title":"The Lost Hero"});
