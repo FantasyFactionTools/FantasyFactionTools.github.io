8617690d16f32fb5e75a9ea5ceec052b6ebc3593
Christopher Chapman <christopher.chapman@disney.com>
2017-10-10 07:20:59 -0700

    grouping the forum code too

--- scripts/lgbt-characters/lgbt-sheet-to-array.py
+++ scripts/lgbt-characters/lgbt-sheet-to-array.py
@@ -15,20 +15,43 @@ reader = csv.DictReader( csvfile, fieldnames, delimiter='\t')
 next(reader, None)  # skip the headers
 
 currentGenre = ""
+lgbt_book_list = {}
 
 for row in reader:
 	if row['genre'] != "":
 		currentGenre = row['genre']
 		jsfile.write("\nauthors[\"" + currentGenre + "\"] = [];\n")
-		bbfile.write("\n\n[color=brown][size=14pt][b]" + currentGenre + "[/b][/size][/color]\n")
+
+		if currentGenre not in lgbt_book_list:
+			lgbt_book_list[currentGenre] = []
 
 	jsfile.write("authors[\"" + currentGenre + "\"].push({\"author\":\"" + row['author'] + "\", \"series\":\"" + row['series'] + "\", \"book-title\":\"" + row['book title'] + "\"});\n")
+	lgbt_book_list[currentGenre].append({"genre":currentGenre, "author":row['author'], "book-title":row['book title'], "series":row['series']})
 
-	bbfile.write("\n    [b]" + row['author'] + "[/b]")
-	if row['book title'] != "":
-		bbfile.write("\n    " + row['book title'])
 
-	if row['series'] != "":
-		bbfile.write("\n    [i]" + row['series'] + "[/i]")
 
-	bbfile.write("\n")
+currentGenre = ""
+currentAuthor = ""
+
+for genre in lgbt_book_list:
+
+	sorted_genre_list = sorted(lgbt_book_list[genre], key=lambda k: k['book-title'])
+
+	for book in sorted_genre_list:
+
+		if currentGenre != book['genre']:
+			currentGenre = book['genre']
+			bbfile.write("\n\n[color=brown][size=14pt][b]" + currentGenre + "[/b][/size][/color]\n")
+
+		if currentAuthor != book['author']:
+			bbfile.write("\n    [b]" + book['author'] + "[/b]")
+			currentAuthor = book['author']
+		
+		if book['book-title'] != "":
+			bbfile.write("\n    " + book['book-title'])
+
+		if book['series'] != "":
+			bbfile.write("\n    [i]" + book['series'] + "[/i]")
+
+		bbfile.write("\n")
+
